d223c2c75420b67b1d88e6e14bdc0e476c04acf7
walterhiggins <walter.higgins@gmail.com>
2012-09-08 12:22:41 +0100

    brief comment

--- rhere.js
+++ rhere.js
@@ -1,3 +1,10 @@
+//
+// a rhino script to expand javascript Here documents
+// into there quoted string equivalents
+//
+// usage:
+// java -jar /opt/rhino/js.jar rhere.js {input-file} > {output-file}
+//
 load("sxoop_strings.js");
 var originalText = readFile(arguments[0]);
 var newText = SXOOP.strings.here(originalText);
